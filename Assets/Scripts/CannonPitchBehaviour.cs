54acc2e25efc571a1ea7c08002b31bdd3fc9c77f
Jeremy Barzas <jbarzas@gmail.com>
2017-06-02 18:22:19 -0500

    turret rotation
    turrent and cannon rotation

---
+++ Assets/Scripts/CannonPitchBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CannonPitchBehaviour : MonoBehaviour
+{
+    public GameObject target;
+
+    private void Update()
+    {
+        transform.LookAt(target.transform);
+    }
+}
