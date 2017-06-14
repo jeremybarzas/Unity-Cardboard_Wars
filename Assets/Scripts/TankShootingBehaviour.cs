4e43bde15c0acd98e087bb218a20041845827959
stackconner <connerstack@gmail.com>
2017-06-14 15:16:59 -0500

    work on shooting behaviour
    this^

---
+++ Assets/Scripts/TankShootingBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankShootingBehaviour : MonoBehaviour
+{
+    public GameObject TankShell;
+    public Transform ShellSpawn;
+    private Vector3 direction;
+    public Rigidbody rb;
+    public AudioSource ShootingAudioSource;
+    public float shootForce = 1.0f;
+    /*afterthoughts go down here v
+    private Random shootCone;
+    */
+    void Fire()
+    {
+        Instantiate(TankShell);
+    }
+	// Use this for initialization
+    void Start()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            Fire();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    direction.z += shootForce;
+	}
+}
