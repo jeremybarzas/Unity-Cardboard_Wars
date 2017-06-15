d00d92bb59f362b857cd09cec9da863b2fa6b8f1
Jeremy Barzas <jbarzas@gmail.com>
2017-06-15 17:57:53 -0500

    merge conner

---
+++ Assets/Scripts/TankAnimationBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator), typeof(Rigidbody))]
+public class TankAnimationBehaviour : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rb;
+    private readonly int speed = Animator.StringToHash("speed");
+	// Use this for initialization
+	void Start ()
+	{
+	    animator = GetComponent<Animator>();
+	    rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetFloat(speed, rb.velocity.magnitude);
+	}
+}
