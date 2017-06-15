80ad3ed5359d09a85e81de37fedb67780e78aaee
stackconner <connerstack@gmail.com>
2017-06-15 10:15:40 -0500

    damage calc work
    this^

--- Assets/Scripts/ProjectileBehaviour.cs
+++ Assets/Scripts/ProjectileBehaviour.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class ProjectileBehaviour : MonoBehaviour
 {
     public ScriptableObject ProjectileScriptableObject;
+    private int Dmg;
+    private GameObject shell;
+    void OnTriggerEnter(Collider other)
+    {
+        if (CompareTag("Player"))
+        {
+            Destroy(shell);
+        }
+    }
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    Dmg = GetComponent<Projectile>().Damage;
 	}
 	
 	// Update is called once per frame
