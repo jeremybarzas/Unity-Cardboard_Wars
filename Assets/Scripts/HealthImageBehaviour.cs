6c2e4345d49a7433401e49541cb3c2bb203dab6b
Jeremy Barzas <jbarzas@gmail.com>
2017-06-15 12:21:01 -0500

    working on shooting and damage

--- Assets/Scripts/HealthImageBehaviour.cs
+++ Assets/Scripts/HealthImageBehaviour.cs
@@ -7,25 +7,69 @@ using UnityEngine.UI;
 
 public class HealthImageBehaviour : MonoBehaviour
 {
-    public OnHealthChanged healthChanged;
+    //public static OnHealthChanged onHealthChanged =  new OnHealthChanged();
+    public TankBehaviour player;
     public Sprite playerHealth;
     public List<Sprite> healthStates;
 
     void Awake()
     {
-        healthChanged.AddListener(SetImage);
+        player.onHealthChanged.AddListener(SetImage);
     }
 
     void Start()
     {
-        healthStates = new List<Sprite>();
+        //healthStates = new List<Sprite>();
     }
 
-    private void SetImage(TankBehaviour tank)
+    public void SetImage(int hp)
     {
-        if (tank.hp == 100)
+        Debug.Log("SetImage Called");
+
+        if (hp == 100)
         {
             playerHealth = healthStates[0];
         }
+        if (hp == 90)
+        {
+            playerHealth = healthStates[1];
+        }
+        if (hp == 80)
+        {
+            playerHealth = healthStates[2];
+        }
+        if (hp == 70)
+        {
+            playerHealth = healthStates[3];
+        }
+        if (hp == 60)
+        {
+            playerHealth = healthStates[4];
+        }
+        if (hp == 50)
+        {
+            playerHealth = healthStates[5];
+        }
+        if (hp == 40)
+        {
+            playerHealth = healthStates[6];
+        }
+        if (hp == 30)
+        {
+            playerHealth = healthStates[7];
+        }
+        if (hp == 20)
+        {
+            playerHealth = healthStates[8];
+        }
+        if (hp == 10)
+        {
+            playerHealth = healthStates[9];
+        }
+        if (hp == 0)
+        {
+            playerHealth = healthStates[10];
+        }
+        GetComponent<Image>().overrideSprite = playerHealth;
     }
 }
--- Assets/Scripts/TankBehaviour.cs
+++ Assets/Scripts/TankBehaviour.cs
@@ -5,40 +5,17 @@ using UnityEngine.Events;
 public class TankBehaviour : MonoBehaviour
 {
     public Tank tank_Attrib;
-    public static OnHealthChanged onHealthChanged;
+    public OnHealthChanged onHealthChanged = new OnHealthChanged();
     public ProjectileBehaviour shellPrefab;
     public int hp;
-
-    void Awake()
-    {
-        onHealthChanged = new OnHealthChanged();
-    }
-
-    void Start()
-    {
-        hp = 100;
-        onHealthChanged.Invoke(this);
-    }
-
-    void OnEnable()
-    {
-        hp = 100;
-        onHealthChanged.Invoke(this);
-    }
 
     void OnTriggerEnter(Collider other)
     {
         if (CompareTag("Player"))
         {
             hp -= shellPrefab.Dmg;
-            onHealthChanged.Invoke(this);
-            Destroy(shellPrefab);
+            onHealthChanged.Invoke(hp);
             Debug.Log(hp);
         }
     }
-
-	void Update ()
-    {
-        
-	}
 }
